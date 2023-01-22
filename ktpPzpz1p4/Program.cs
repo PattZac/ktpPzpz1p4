e6f77f53b62f3cb553498b565898095cdc2bac52
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 15:49:47 +0100

    Add files via upload

---
+++ ktpPzpz1p4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktpPzpz1p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijenu: ");
+            float n = float.Parse(Console.ReadLine());
+            Console.WriteLine("Cijenu bez PDV-a: "+n+"."+"\r\nCijenu s PDV-om: "+n*1.25+".");
+            Console.ReadKey();
+        }
+    }
+}
